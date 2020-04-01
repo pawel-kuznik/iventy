2c9d9c24eefe3c8701be185b55243d5e1a72c9b9
Pawel <pawel.kuznik@gmail.com>
2020-04-01 21:35:43 +0200

    update documentation of the main file

--- iventy.ts
+++ iventy.ts
@@ -1,6 +1,13 @@
-// <reference path="./lib/Emitter.ts" />
-// <reference path="./lib/Event.ts" />
+/**
+ *  This is an entry file for the whole library. This file exposes (and kicks in
+ *  the compilation process) all of the public classes of the library. This means
+ *  that if something should be exposed outside, it should be exported via this
+ *  file.
+ *
+ *  @author     Paweł Kuźnik <pawel.kuznik@gmail.com>
+ */
 
+// export all of the public classes.
 export { Event } from './lib/Event';
 export { Emitter } from './lib/Emitter';
 export { EventHandler } from './lib/EventHandler';
